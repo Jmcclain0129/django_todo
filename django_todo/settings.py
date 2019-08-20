6fb627cb31896052c3849bf0e285a7bc06a57eb4
jmcclain0129 <ubuntu@ip-172-31-81-183.ec2.internal>
2019-08-20 14:02:11 +0000

    fixed env variable error by adding additional allowed host

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 import dj_database_url
 
+if os.environ.get('DEVELOPMENT'):
+    development = True
+else:
+    development = False
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -28,7 +33,8 @@ SECRET_KEY = '0)co@q+3^t)%$+k+es!4u7$iixgd-ljorw&e07+%691y)wuk(o'
 DEBUG = True
 
 ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'),
-                 os.environ.get('HOSTNAME')]
+                 os.environ.get('HOSTNAME'),
+                 '5eb7dc7669e348f79248e2115ed3a8b3.vfs.cloud9.us-east-1.amazonaws.com']
 
 
 # Application definition
@@ -77,8 +83,15 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-
-DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+if development:
+        DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            }
+        }
+else:
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
 
 
 # Password validation
